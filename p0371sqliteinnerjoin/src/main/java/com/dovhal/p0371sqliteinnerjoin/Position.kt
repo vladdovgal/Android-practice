bfc41d838e706b4c8ddad1ac6d5d19112b7b9c84
vladdovgal <vladdovgal@gmail.com>
2020-07-12 23:55:52 +0300

    some refactor java to kotlin

---
+++ p0371sqliteinnerjoin/src/main/java/com/dovhal/p0371sqliteinnerjoin/Position.kt
@@ -0,0 +1,10 @@
+package com.dovhal.p0371sqliteinnerjoin
+
+enum class Position(salary: Int) {
+    DIRECTOR(15000),
+    PROGRAMMER(11000),
+    ACCOUNTANT(9000),
+    CLEANER(6000);
+
+    var salary: Int = 0;
+}
